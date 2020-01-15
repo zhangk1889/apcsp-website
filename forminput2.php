cf121f9a02f5742d5e47a8d44b8d4a103d223fe6
Kevin <zhangk1889@my.sduhsd.net>
2020-01-15 13:24:34 -0800

    php

---
+++ forminput2.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Form Input 2</title>
+  </head>
+
+
+  <body>
+
+    <h1>Form Input - Demo 2</h1>
+    <p>Demo of how to take form input and pass it to a program - all in a single page</p>
+
+    <?php
+       // define variables and set to empty values
+       $arg1 = $arg2 = $output = $retc = "";
+
+       if ($_SERVER["REQUEST_METHOD"] == "POST") {
+         $arg1 = test_input($_POST["arg1"]);
+         $arg2 = test_input($_POST["arg2"]);
+         exec("/usr/lib/cgi-bin/sp1a/areaOfCircle2 " . $arg1 . " " . $arg2, $output, $retc); 
+       }
+
+       function test_input($data) {
+         $data = trim($data);
+         $data = stripslashes($data);
+         $data = htmlspecialchars($data);
+         return $data;
+       }
+    ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      Arg1: <input type="text" name="arg1"><br>
+      Arg2: <input type="text" name="arg2"><br>
+      <br>
+      <input type="submit" value="Go!">
+    </form>
+
+    <?php
+       // only display if return code is numeric - i.e. exec has been called
+       if (is_numeric($retc)) {
+         echo "<h2>Your Input:</h2>";
+         echo $arg1;
+         echo "<br>";
+         echo $arg2;
+         echo "<br>";
+       
+         echo "<h2>Program Output (an array):</h2>";
+         foreach ($output as $line) {
+           echo $line;
+           echo "<br>";
+         }
+       
+         echo "<h2>Program Return Code:</h2>";
+         echo $retc;
+       }
+    ?>
+    
+  </body>
+</html>
